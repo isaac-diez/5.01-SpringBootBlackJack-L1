07bfe9a0956f24d0e9a3472d37a1dbfeb170f9ff
isaac-diez <isaac.diez@proton.me>
2025-05-02 20:15:23 +0200

    Feat: improved/completed Player class. Added PlayerType enum

--- src/main/java/cat/itacademy/s05/t01/n01/model/Player.java
+++ src/main/java/cat/itacademy/s05/t01/n01/model/Player.java
@@ -1,9 +1,40 @@
 package cat.itacademy.s05.t01.n01.model;
 
-public class Player extends Person{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
 
-    public Player(String id, String name) {
-        super(id, name);
+@Entity
+@Table(name="players")
+
+public class Player {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private PlayerType type;
+
+    public Player(String name, PlayerType type) {
+        this.name = name;
+        this.type = type;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public PlayerType getType() {
+        return type;
+    }
 }
---
+++ src/main/java/cat/itacademy/s05/t01/n01/model/PlayerType.java
@@ -0,0 +1,5 @@
+package cat.itacademy.s05.t01.n01.model;
+
+public enum PlayerType {
+    PLAYER, CROUPIER
+}
