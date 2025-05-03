357c32d32bfdd1bd17a3793632840bbb0839051f
isaac-diez <isaac.diez@proton.me>
2025-05-03 12:55:35 +0200

    Feat: Implemented Class Hand to collect all cards and calculate hand value

--- src/main/java/cat/itacademy/s05/t01/n01/model/Hand.java
+++ src/main/java/cat/itacademy/s05/t01/n01/model/Hand.java
@@ -1,4 +1,48 @@
 package cat.itacademy.s05.t01.n01.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hand {
+
+    private List<Card> hand = new ArrayList<>();
+    private int result = 0;
+
+    public Hand() { }
+
+    public void addCardtoHand(Card card) {
+        hand.add(card);
+    }
+
+    public int getHandValue() {
+
+        int total = 0;
+        int aceCount = 0;
+
+        for (Card card : hand) {
+            total += card.getValue();
+            if (card.isAce()){
+                aceCount++;
+            }
+        }
+
+        while (total > 21 && aceCount > 0) {
+            total -= 10;
+            aceCount--;
+        }
+
+        return total;
+    }
+
+    public boolean isBust() {
+        return getHandValue() > 21;
+    }
+
+    public boolean isBlackjack() {
+        return hand.size() == 2 && getHandValue() == 21;
+    }
+
+    public List<Card> getCards() {
+        return hand;
+    }
 }
