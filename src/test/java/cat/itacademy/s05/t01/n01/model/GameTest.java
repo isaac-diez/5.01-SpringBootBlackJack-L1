65439b05e14fc403c54b4cc5bf6578e3f63b77c6
isaac-diez <isaac.diez@proton.me>
2025-05-06 13:25:05 +0200

    Refactor: improved game tests that work

--- src/test/java/cat/itacademy/s05/t01/n01/model/GameTest.java
+++ src/test/java/cat/itacademy/s05/t01/n01/model/GameTest.java
@@ -14,35 +14,37 @@ class GameTest {
 
         Player Isaac = new Player("Isaac", PlayerType.PLAYER);
 
-//        newDeck.populateDeck();
-//        Deck shuffledDeck = newDeck.shuffleDeck();
-
         Game gameTest = new Game(Isaac);
+
+        int i = 0;
+        for (Card card : gameTest.getDeck().getDeckList()) {
+            System.out.println("key: " + i + " card: " + gameTest.getDeck().getDeckList().get(i).getName() +
+                    " of " + gameTest.getDeck().getDeckList().get(i).getSuit());
+            i++;
+        }
+        Assertions.assertEquals(48, newDeck.getDeckSize());
 
         gameTest.gameInit();
 
         Assertions.assertEquals(2,gameTest.getPlayerHand(PlayerType.PLAYER).getHandList().size());
 
-//        int j=0;
-//        for (Card card : gameTest.getPlayerHand(PlayerType.PLAYER).getHandList()) {
-//            System.out.println("key: " + j + " value: " + gameTest.getPlayerHand(PlayerType.PLAYER).getHandList().get(j).getName()
-//                    + " of " + gameTest.getPlayerHand(PlayerType.PLAYER).getHandList().get(j).getSuit());
-//            j++;
-//        }
+        int j=0;
+        for (Card card : gameTest.getPlayerHand(PlayerType.PLAYER).getHandList()) {
+            System.out.println("key: " + j + " value: " + gameTest.getPlayerHand(PlayerType.PLAYER).getHandList().get(j).getName()
+                    + " of " + gameTest.getPlayerHand(PlayerType.PLAYER).getHandList().get(j).getSuit());
+            j++;
+        }
 
         Assertions.assertEquals(2,gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList().size());
 
-//        j=0;
-//        for (Card card : gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList()) {
-//            System.out.println("key: " + j + " value: " + gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList().get(j).getName()
-//                    + " of " + gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList().get(j).getSuit());
-//            j++;
-//        }
+        j=0;
+        for (Card card : gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList()) {
+            System.out.println("key: " + j + " value: " + gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList().get(j).getName()
+                    + " of " + gameTest.getPlayerHand(PlayerType.CROUPIER).getHandList().get(j).getSuit());
+            j++;
+        }
 
         Assertions.assertEquals(44,gameTest.getDeck().getDeckSize());
-
-
-
 
 
     }
