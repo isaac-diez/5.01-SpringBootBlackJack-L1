83614278779eabd6d550f7de8ea3097181153507
isaac diez <isaac.diez@proton.me>
2025-05-15 19:52:17 +0200

    Update: Refactored RankingController @RequestMapping url

--- src/main/java/cat/itacademy/s05/t01/n01/controller/RankingController.java
+++ src/main/java/cat/itacademy/s05/t01/n01/controller/RankingController.java
@@ -4,12 +4,14 @@ import cat.itacademy.s05.t01.n01.model.Player;
 import cat.itacademy.s05.t01.n01.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class RankingController {
 
     @Autowired
